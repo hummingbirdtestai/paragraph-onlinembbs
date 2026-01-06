97a9b5ea075f2323bd459a7b381a4db56e2a6e32
hummingbirdtestai <hummingbirdtestai@gmail.com>
2026-01-06 13:44:50 +0530

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,15 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.resolver.alias = {
-  '@': path.resolve(__dirname, './'),
+config.resolver = {
+  ...config.resolver,
+  alias: {
+    '@': path.resolve(__dirname),
+  },
 };
 
 module.exports = config;
