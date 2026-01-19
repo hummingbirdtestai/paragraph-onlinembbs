17a8ca783158f26935ab7d32080347a3f3492719
hummingbirdtestai <hummingbirdtestai@gmail.com>
2026-01-19 17:16:43 +0530

    Publish application

---
+++ app/uhs-pyq-question-bank.tsx
@@ -0,0 +1,8 @@
+//uhs-pyq-question-bank.tsx
+// app/uhs-pyq-question-bank.tsx
+
+import UhsPyqCurriculumTable from "@/components/UhsPyqCurriculumTable";
+
+export default function UhsPyqQuestionBankScreen() {
+  return <UhsPyqCurriculumTable />;
+}
