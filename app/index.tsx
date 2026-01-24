13e477f4af453a31b32ab4f64a83ba536bcb1ae7
hummingbirdtestai <hummingbirdtestai@gmail.com>
2026-01-24 12:06:56 +0530

    Updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -30,9 +30,10 @@ export default function Index() {
    * WEB VIDEOS (Bunny Stream iframe URLs)
    * These are used ONLY on web layouts.
    */
-  const videos = {
-    hero: "https://iframe.mediadelivery.net/play/562001/fd252cc5-912d-4134-86f9-9732f883facf",
-  };
+const videos = {
+  hero: "https://iframe.mediadelivery.net/embed/562001/fd252cc5-912d-4134-86f9-9732f883facf",
+};
+
 
   return (
     <MainLayout>
