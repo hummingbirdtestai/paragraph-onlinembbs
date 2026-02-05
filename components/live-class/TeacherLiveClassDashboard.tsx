4a093566e875da82998eec718dce4331891fdb25
hummingbirdtestai <hummingbirdtestai@gmail.com>
2026-02-05 18:56:29 +0530

    Updated TeacherLiveClassDashboard.tsx

---
+++ components/live-class/TeacherLiveClassDashboard.tsx
@@ -0,0 +1,10 @@
+//TeacherLiveClassDashboard.tsx
+import { View, Text } from 'react-native';
+
+export default function TeacherLiveClassDashboard() {
+  return (
+    <View>
+      <Text>Teacher Live Class Dashboard</Text>
+    </View>
+  );
+}
