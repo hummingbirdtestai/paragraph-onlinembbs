8ed0e31afcae3a6cd7df8804ac9ef072fb41f673
hummingbirdtestai <hummingbirdtestai@gmail.com>
2026-02-06 00:14:28 +0530

    Updated _layout.tsx

--- app/live-class/_layout.tsx
+++ app/live-class/_layout.tsx
@@ -1,11 +1,6 @@
 // app/live-class/_layout.tsx
 import { Slot } from 'expo-router';
-import MainLayout from '@/components/MainLayout';
 
 export default function LiveClassLayout() {
-  return (
-    <MainLayout>
-      <Slot />
-    </MainLayout>
-  );
+  return <Slot />;
 }
