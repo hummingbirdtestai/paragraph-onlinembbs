3007ceb05b4c533c98897b29593dbf11db6536d1
hummingbirdtestai <hummingbirdtestai@gmail.com>
2026-02-05 18:56:10 +0530

    Updated StudentLiveClassList.tsx

---
+++ components/live-class/StudentLiveClassList.tsx
@@ -0,0 +1,10 @@
+//StudentLiveClassList.tsx
+import { View, Text } from 'react-native';
+
+export default function StudentLiveClassList() {
+  return (
+    <View>
+      <Text>Student Live Class List</Text>
+    </View>
+  );
+}
